f22769e1f1a12b0db9f8846fd4bace22f2f6df94
UCHINO Tatsunori <uchino.tatsunori@h.mbox.nagoya-u.ac.jp>
2016-10-26 19:59:19 +0900

    LDAP関連のライブラリ追加

---
+++ app/ldap_lib.php
@@ -0,0 +1,52 @@
+<?php
+function GetUserDN($ad, $samaccountname, $basedn) {
+    $attributes = array('dn');
+    $result = ldap_search($ad, $basedn,
+        "(uid={$samaccountname})", $attributes);
+    if ($result === FALSE) { return ''; }
+    $entries = ldap_get_entries($ad, $result);
+    if ($entries['count']>0) { return $entries[0]['dn']; }
+    else { return ''; };
+}
+
+function GetGroupDN($ad, $samaccountname, $basedn) {
+    $attributes = array('dn');
+    $result = ldap_search($ad, $basedn,
+        "(cn={$samaccountname})", $attributes);
+    if ($result === FALSE) { return ''; }
+    $entries = ldap_get_entries($ad, $result);
+    if ($entries['count']>0) { return $entries[0]['dn']; }
+    else { return ''; };
+}
+
+function CheckGroupEx($ad, $userdn, $groupdn) {
+    $attributes = array('memberof');
+    $result = ldap_read($ad, $userdn, '(objectClass=*)', $attributes);
+    if ($result === FALSE) { return FALSE; };
+    $entries = ldap_get_entries($ad, $result);
+    if ($entries['count'] <= 0) { return FALSE; };
+    if (empty($entries[0]['memberof'])) { return FALSE; } else {
+        for ($i = 0; $i < $entries[0]['memberof']['count']; $i++) {
+            if ($entries[0]['memberof'][$i] == $groupdn) { return TRUE; }
+            elseif (CheckGroupEx($ad, $entries[0]['memberof'][$i], $groupdn)) { return TRUE; };
+        };
+    };
+    return FALSE;
+}
+
+function GetGroupMembers($ad, $samaccountname, $basedn) {
+    $attributes = array('memberUid');
+    $result = ldap_search($ad, $basedn,
+        "(cn={$samaccountname})", $attributes);
+    if ($result === FALSE) { return []; }
+    $entries = ldap_get_entries($ad, $result);
+    if ($entries['count']>0) {
+      $ret = $entries[0]["memberuid"];
+      unset($ret["count"]);
+      return $ret;
+    }
+    else { return []; };
+}
+
+
+?>

c3f7f5873d39a8f87b543f17eca9ddf57ec0f3f3
UCHINO Tatsunori <uchino.tatsunori@h.mbox.nagoya-u.ac.jp>
2016-10-26 18:44:24 +0900

    index.php変更
    仮管理画面追加

--- app/index.php
+++ app/index.php
@@ -1,3 +1,26 @@
 <?php
-header("Location: login.html",TRUE,307);
+
+function redirectToLoginPage() {
+    header("Location: login.php",TRUE,307);
+    print("Redirecting to the login page...\n");
+    exit();
+}
+
+session_start();
+if(!$_SESSION || empty($_SESSION["state"])) {
+    redirectToLoginPage();
+}
+
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>管理画面</title>
+    </head>
+    <body>
+        <h1>管理画面</h1>
+        <p><a href="login.php">ログイン</a></p>
+        <p><a href="logout.php">ログアウト</a></p>
+    </body>
+</html>

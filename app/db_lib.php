7d2c2cb219598620135d3f62a9c38289996a6210
UCHINO Tatsunori <uchino.tatsunori@h.mbox.nagoya-u.ac.jp>
2016-10-27 10:37:47 +0900

    デバッグ: 鍵取得クエリ結果が空行ならデバッグ情報を出力するように

--- app/db_lib.php
+++ app/db_lib.php
@@ -14,9 +14,16 @@ function GetKeyListFromDB($dbServer, $dbUser, $dbPass, $dbName, $userName) {
     try {
         $dbQuery = $dbObj->prepare("select key_name,key_type,key_content,key_comment from pubkeys where user_index in (select user_index from user_name where user_name = ?)");
         if(!$dbQuery->execute([$userName])) return ["succeeded" => false, "message" => "公開鍵のリストを取得するクエリに失敗しました"];
+        if($dbQuery->rowCount() != 0)
         return ["succeeded" => true, "data" => array_map(function($row) {
             return ["name" => $row["key_name"], "type" => $row["key_type"], "content" => $row["key_content"], "comment" => $row["key_comment"]];
         }, $dbQuery->fetchAll())];
+        else {
+            ob_start();
+            $dbQuery->debugDumpParams();
+            $info = ob_get_clean();
+            return ["succeeded" => false, "message" => $info];
+        }
     } catch(PDOException $e) {
         return ["succeeded" => false, "message" => "公開鍵のリストを取得するクエリに失敗しました"];
     }   

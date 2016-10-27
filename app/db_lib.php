7969c9c37222f68b1674a975c513a95a06b1edc8
UCHINO Tatsunori <uchino.tatsunori@g.sp.m.is.nagoya-u.ac.jp>
2016-10-27 11:38:13 +0900

    db_lib.php: 公開鍵データを入れるハッシュ名を間違えてたので修正
    ×data○keys

--- app/db_lib.php
+++ app/db_lib.php
@@ -15,7 +15,7 @@ function GetKeyListFromDB($dbServer, $dbUser, $dbPass, $dbName, $userName) {
         $dbQuery = $dbObj->prepare("select key_name,key_type,key_content,key_comment from pubkeys where user_index in (select user_index from user_name where user_name = ?)");
         if(!$dbQuery->execute([$userName])) return ["succeeded" => false, "message" => "公開鍵のリストを取得するクエリに失敗しました"];
         $result = $dbQuery->fetchAll(); 
-        return ["succeeded" => true, "data" => array_map(function($row) {
+        return ["succeeded" => true, "keys" => array_map(function($row) {
             return ["name" => $row["key_name"], "type" => $row["key_type"], "content" => $row["key_content"], "comment" => $row["key_comment"]];
         }, $result)];
     } catch(PDOException $e) {

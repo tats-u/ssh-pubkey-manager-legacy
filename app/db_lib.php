4dd0582d678d1bc757d5a581da263469650bb67c
UCHINO Tatsunori <uchino.tatsunori@g.sp.m.is.nagoya-u.ac.jp>
2016-10-27 11:37:30 +0900

    デバッグ削除

--- app/db_lib.php
+++ app/db_lib.php
@@ -9,17 +9,12 @@ function GetKeyListFromDB($dbServer, $dbUser, $dbPass, $dbName, $userName) {
     try {
         $dbObj = new PDO($dsn, $dbUser, $dbPass);
     } catch(PDOException $e) {
-        return ["succeeded" => false, "message" => "データベースに接続できませんでした\n" . $e.getMessage()];
+        return ["succeeded" => false, "message" => "データベースに接続できませんでした"];
     }
     try {
         $dbQuery = $dbObj->prepare("select key_name,key_type,key_content,key_comment from pubkeys where user_index in (select user_index from user_name where user_name = ?)");
         if(!$dbQuery->execute([$userName])) return ["succeeded" => false, "message" => "公開鍵のリストを取得するクエリに失敗しました"];
         $result = $dbQuery->fetchAll(); 
-        ob_start();
-        $dbQuery->debugDumpParams();
-        var_dump($result);
-        $info = ob_get_clean();
-        file_put_contents("php://stderr", "--- DEBUG MESSAGE BEGIN ---\n" . $info . "\n--- DEBUG MESSAGE END ---\n");
         return ["succeeded" => true, "data" => array_map(function($row) {
             return ["name" => $row["key_name"], "type" => $row["key_type"], "content" => $row["key_content"], "comment" => $row["key_comment"]];
         }, $result)];

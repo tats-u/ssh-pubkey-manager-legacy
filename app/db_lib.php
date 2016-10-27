1e1b77bae2dd6ce8080a6e411fd94ee2799ea74e
UCHINO Tatsunori <uchino.tatsunori@h.mbox.nagoya-u.ac.jp>
2016-10-27 10:43:00 +0900

    デバッグ変更

--- app/db_lib.php
+++ app/db_lib.php
@@ -14,16 +14,15 @@ function GetKeyListFromDB($dbServer, $dbUser, $dbPass, $dbName, $userName) {
     try {
         $dbQuery = $dbObj->prepare("select key_name,key_type,key_content,key_comment from pubkeys where user_index in (select user_index from user_name where user_name = ?)");
         if(!$dbQuery->execute([$userName])) return ["succeeded" => false, "message" => "公開鍵のリストを取得するクエリに失敗しました"];
-        if($dbQuery->rowCount() != 0)
+        $result = $dbQuery->fetchAll(); 
+        ob_start();
+        $dbQuery->debugDumpParams();
+        var_dump($result);
+        $info = ob_get_clean();
+        file_put_contents("php://stderr", "--- DEBUG MESSAGE BEGIN ---\n" . $info . "\n--- DEBUG MESSAGE END ---\n");
         return ["succeeded" => true, "data" => array_map(function($row) {
             return ["name" => $row["key_name"], "type" => $row["key_type"], "content" => $row["key_content"], "comment" => $row["key_comment"]];
-        }, $dbQuery->fetchAll())];
-        else {
-            ob_start();
-            $dbQuery->debugDumpParams();
-            $info = ob_get_clean();
-            return ["succeeded" => false, "message" => $info];
-        }
+        }, $result)];
     } catch(PDOException $e) {
         return ["succeeded" => false, "message" => "公開鍵のリストを取得するクエリに失敗しました"];
     }   
